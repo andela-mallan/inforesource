7efdb14547070d95e8a146a989707b35bdc8f8af
andela-mallan <allanmwesigwa@Allans-MacBook-Pro.local>
2017-04-23 18:25:07 +0300

    Adds a demo for control structures e.g. @if and @for

--- getting-started/resources/views/welcome.blade.php
+++ getting-started/resources/views/welcome.blade.php
@@ -4,6 +4,16 @@
   <div class="row">
     <div class="col-md-6">
       <h1> Some Content </h1>
+
+      @if(true)
+        <p>Displays if true</p>
+      @else
+        <p>Displays if false</p>
+      @endif
+
+      @for($i=1; $i<5; $i++)
+        <p>{{ $i }} iteration</p>
+      @endfor
     </div>
   </div>
 @endsection
