7dc3c58d08e58c9926ce647d59d147fb83320b0b
andela-mallan <allanmwesigwa@Allans-MacBook-Pro.local>
2017-04-23 18:18:29 +0300

    Adds some views as partials

--- getting-started/resources/views/welcome.blade.php
+++ getting-started/resources/views/welcome.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.main')
 
 @section('content')
-  <p>Some Content</p>
+  <div class="row">
+    <div class="col-md-6">
+      <h1> Some Content </h1>
+    </div>
+  </div>
 @endsection
