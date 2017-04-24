630ac59b9fb3f12d38b8dbcf9916e01c7d34d471
andela-mallan <allanmwesigwa@Allans-MacBook-Pro.local>
2017-04-24 11:50:10 +0300

    Adds a demo of XSS

--- getting-started/resources/views/welcome.blade.php
+++ getting-started/resources/views/welcome.blade.php
@@ -3,8 +3,7 @@
 @section('content')
   <div class="row">
     <div class="col-md-6">
-      <h1> Some Content </h1>
-
+      <h1> Control Structures </h1>
       @if(true)
         <p>Displays if true</p>
       @else
@@ -14,6 +13,10 @@
       @for($i=1; $i<5; $i++)
         <p>{{ $i }} iteration</p>
       @endfor
+
+      <h1> XSS </h1>
+      {{ "<script>alert('Hello!');</script>" }}
+      {!! "<script>alert('Hello!');</script>" !!}
     </div>
   </div>
 @endsection
